7b91d65a3e70a386925b96136e3196a3d2002326
Philipp Sommer <95475743+psomm@users.noreply.github.com>
2023-01-16 16:40:50 +0000

    modified main.py 04_01

--- Ex_Files/04_01_begin/main.py
+++ Ex_Files/04_01_begin/main.py
@@ -1,6 +1,11 @@
 import requests
 
-response = requests.get(
-    "http://api.worldbank.org/v2/countries/USA/indicators/SP.POP.TOTL?per_page=5000&format=json")
+r = requests.get(
+    "http://api.worldbank.org/v2/countries/USA/indicators/SP.POP.TOTL?per_page=5000&format=json",
+    timeout=3)
 
-last_twenty_years = response.json()[1][:20]
+last_twenty_years = r.json()[1][:20]
+
+for year in last_twenty_years:
+    display_width = year["value"] // 5_000_000
+    print(f"{year['date']} {year['value']:,} Einwohner {'=' * display_width}")
